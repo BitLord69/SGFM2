b19b07737b564995662969d891a33db47c48b5dd
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-12-03 12:00:35 +0100

    created LandingPageComponent
    Co-Authored-By: nekcoj <55090544+nekcoj@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>Super Galaxy Face Melter</h1>
+  <div class="container">
+    <LandingPageComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPageComponent from './components/LandingPageComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPageComponent
   }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+.container {
+  display: flex;
+  justify-content: center;
+}
+h1 {
+  text-align: left;
+  margin-left: 1.5em;
+  word-spacing: -0.5em;
+  font-family: 'Press Start 2P', cursive;
+}
+</style>
---
+++ frontend/src/components/LandingPageComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gameWindow">
+    <button>Play a game</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPageComponent'
+}
+</script>
+
+<style scoped>
+  .gameWindow{
+    width: 80vw;
+    height: 70vh;
+    background: linear-gradient(90deg, rgba(226,195,166,1) 0%, rgba(59,23,4,1) 50%, rgba(226,195,166,1) 100%);
+    position: relative;
+    justify-content: center;
+    display: flex;
+  }
+  button{
+    position: absolute;
+    bottom: 10em;
+    height: 5em;
+    width: 20em;
+    background-color: #e2c3a6;
+    border: 0.2em solid #3b1704;
+    box-shadow: 0.2em 0.2em black;
+    border-radius: 1em;
+    font-size: 1rem;
+    color: #3b1704;
+    font-family: 'Press Start 2P', cursive;
+  }
+</style>

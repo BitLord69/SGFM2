5e8f5c9547326d212bcd8135e4a415eeb61d7bb1
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-12-16 16:14:40 +0100

    Login and logout working
    Co-Authored-By: Janis Karlsson <janis_k@hotmail.com>

---
+++ backend/database-server/middleware/api/Auth.js
@@ -0,0 +1,36 @@
+// const db = require("./utils/DBHelper").getInstance();
+const router = require('express').Router()
+const user = require('../../entities/User')
+
+router.post("/login", async(req, res) => {
+  console.log("req.sess.user", req.session.user);
+  if (req.session.user) {
+    return res.json({ error: 'Already logged in!' });
+  }
+
+  const { email, password } = req.body;
+  
+  const result = await user.login(email, password);
+
+  console.log("result: ", result);
+  // Log in by adding user to the session
+  req.session.user = result;
+  delete req.session.user.password
+  return res.json(req.session.user);
+});
+
+router.post("/logout", (req, res) => {
+  if (!req.session.user) {
+    return res.json({ error: "Already logged out!" });
+  }
+
+  // Log out the user by deleting the session.user property
+  delete req.session.user;
+  res.json({ success: "Logged out!" });
+});
+
+router.get('/whoami', (req, res) => {
+  res.json(req.session.user || { error: 'No user logged in!' });
+});
+
+module.exports = router

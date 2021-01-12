b1856a1bd1036e6f9fb0d9ecafa7ade4177159c9
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2021-01-12 23:47:29 +0100

    added some properties in query and added time since last game

--- frontend/src/components/LatestGame.vue
+++ frontend/src/components/LatestGame.vue
@@ -1,27 +1,36 @@
 <template>
   <div id="latestGame">
-    <div class="p-text-center">Your latest played game</div>
-    <div class="leaderboard-content d-flex p-jc-between">
-      <div class="p-text-center" v-if="latestGame[0].winner == 0">You won against</div>
-      <div class="p-text-center" v-if="latestGame[0].winner == 1">You lost against</div>
-      <div class="p-text-center" v-if="latestGame[0].winner == 2">You tied against</div>
-      <div class="p-text-center">{{latestGame[0].opponent}}</div>
+    <div class="p-text-center p-mb-1">Your latest played game</div>
+    <div class="latestGame-content p-m-0 p-grid">
+      <div class="p-col-2 p-p-0 p-my-auto"><img class="avatar p-my-auto" :src="'/avatar/' + currentUser.avatar + '.png'" alt="" /></div>
+      <div class="p-col-9 p-p-0 p-my-auto">{{currentUser.username}}</div>
+      <div class="p-col-1 p-p-0 p-my-auto">{{latestGame[0].myPoints}}</div>
+      <div class="divider p-m-1"></div>
+      <div class="p-col-2 p-p-0 p-my-auto"><img class="avatar" :src="'/avatar/' + latestGame[0].oppAvatar + '.png'" alt="" /></div>
+      <div class="p-col-9 p-p-0 p-my-auto">{{latestGame[0].opponent}}</div>
+      <div class="p-col-1 p-p-0 p-my-auto">{{latestGame[0].oppPoints}}</div>
     </div>
+    <div class="p-text-center p-mt-1">{{moment(latestGame[0].time).fromNow(true)}} ago</div>
   </div>
 </template>
 <script>
-import GameHandler from "../modules/GameHandler";
+import GameHandler from "@/modules/GameHandler";
+import UserHandler from "@/modules/UserHandler";
+import moment from 'moment';
 
 export default {
 
   async setup() {
 
   const { getLatestGame, latestGame } = GameHandler();
+  const { currentUser } = UserHandler();
+
   await getLatestGame();
-  console.log("latestGame:", );
 
   return {
     latestGame,
+    currentUser,
+    moment
   }
   }
 };
@@ -33,14 +42,27 @@ export default {
   background-color: #e2c3a6;
   padding: 1%;
   border: 2px solid #3b1704;
+  border-radius: 5px;
+  font-family: "Yanone Kaffeesatz", sans-serif;
 }
 
-.leaderboard-content {
+.latestGame-content {
   background-color: #eeeeee;
-  display: grid;
-  grid-template-rows: repeat(1, 1fr);
   width: 15vw;
   padding: 3%;
   border-radius: 5px;
+}
+.divider {
+  width:100%;
+  border-bottom: 1px solid #bebebe;
+}
+.avatar {
+  display: block;
+  margin-top: auto;
+  margin-bottom: auto;
+  width: 2em;
+  border: 2px solid #3b1704;
+  border-radius: 50%;
+  vertical-align: baseline
 }
 </style>
